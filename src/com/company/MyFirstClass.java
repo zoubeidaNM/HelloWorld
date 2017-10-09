51cafea42725a72a78ad5ad9805eab8f89f934d1
zoubeidaNM <zoumenz@yahoo.com>
2017-10-09 11:03:51 -0400

    Initial commit

---
+++ src/com/company/MyFirstClass.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("My name is Zoubeida.");
+        System.out.println("Today's date: 10/9/2017.");
+    }
+}
